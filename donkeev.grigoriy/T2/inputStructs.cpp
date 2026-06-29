4473108f17dbb42a38854474ba9bc37357a850de
grisha-git <donkeevgrisa@gmail.com>
2026-06-29 23:42:17 +0300

    manip struct imolementation

--- donkeev.grigoriy/T2/inputStructs.cpp
+++ donkeev.grigoriy/T2/inputStructs.cpp
@@ -1,5 +1,21 @@
 #include "inputStructs.hpp"
 
+donkeev::IOGuard::IOGuard(std::basic_ios< char >& s):
+  s_(s),
+  precision_(s.precision()),
+  width_(s.width()),
+  flags_(s.flags()),
+  fill_(s.fill())
+{}
+
+donkeev::IOGuard::~IOGuard()
+{
+  s_.precision(precision_);
+  s_.width(width_);
+  s_.flags(flags_);
+  s_.fill(fill_);
+}
+
 std::istream& donkeev::operator>>(std::istream& in, DblSci& val)
 {
   std::istream::sentry s(in);
