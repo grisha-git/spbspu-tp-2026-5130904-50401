b9bc4105457fd9569cc00b64bc2176251f0f4f26
grisha-git <donkeevgrisa@gmail.com>
2026-06-30 16:45:23 +0300

    input and output the point

--- donkeev.grigoriy/T3/polygon.cpp
+++ donkeev.grigoriy/T3/polygon.cpp
@@ -1,5 +1,7 @@
 #include "polygon.hpp"
 
+#include <iostream>
+
 donkeev::IOGuard::IOGuard(std::basic_ios<char>& s):
   s_(s),
   precision_(s.precision()),
@@ -14,4 +16,30 @@ donkeev::IOGuard::~IOGuard()
   s_.width(width_);
   s_.flags(flags_);
   s_.fill(fill_);
+}
+
+std::istream& donkeev::operator>>(std::istream& is, Point& p)
+{
+  std::istream::sentry sentry(is);
+  if (!sentry)
+  {
+    return is;
+  }
+
+  IOGuard guard(is);
+  char last = 0;
+  Point temp = {0, 0};
+  is >> Delimiter{'(', last} >> temp.x >> Delimiter{';', last} >> temp.y >> Delimiter{')', last};
+
+  if (is)
+  {
+    p = temp;
+  }
+  return is;
+}
+
+std::ostream& donkeev::operator<<(std::ostream& os, const Point& p)
+{
+  os << '(' << p.x << ';' << p.y << ')';
+  return os;
 }
